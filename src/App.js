41156312db4271fbf25b8d1196dff45a30403bcf
ikechukwuakuma316 <ikechukwuakuma316@gmail.com>
2023-05-02 10:38:53 +0100

    The Launch Sandbox for amplify studio

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { DataStore } from '@aws-amplify/datastore';
+import { Todo } from './models';
 
 function App() {
+  async function deleteItem() { 
+    const modelToDelete = await DataStore.query(Todo, "e512f814-c598-4377-b59f-14093e89ac00");
+    DataStore.delete(modelToDelete);
+  }
+  async function update() {
+    const original = await DataStore.query(Todo, "e512f814-c598-4377-b59f-14093e89ac00")
+
+await DataStore.save(
+  Todo.copyOf(original, item => {
+  // Update the values on {item} variable to update DataStore entry
+  item.name = `title ${Date.now()}`;
+})
+);
+  }
+  async function showTodos() {
+    const models = await DataStore.query(Todo);
+      console.log(models);
+  }
+  
+  async function addTodo() {
+    await DataStore.save(
+      new Todo({
+      "title": "Lorem ipsum dolor sit amet",
+      "comments": [],
+      "content": "Lorem ipsum dolor sit amet"
+    })
+  );
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={addTodo}>Add</button>
+        <button onClick={showTodos}>Show Todos</button>
+        <button onClick={update}>Update</button>
+        <button onClick={deleteItem}>Delete</button>
       </header>
     </div>
   );
---
+++ src/models/index.d.ts
@@ -0,0 +1,69 @@
+import { ModelInit, MutableModel, __modelMeta__, ManagedIdentifier } from "@aws-amplify/datastore";
+// @ts-ignore
+import { LazyLoading, LazyLoadingDisabled, AsyncCollection } from "@aws-amplify/datastore";
+
+
+
+
+
+type EagerTodo = {
+  readonly [__modelMeta__]: {
+    identifier: ManagedIdentifier<Todo, 'id'>;
+    readOnlyFields: 'createdAt' | 'updatedAt';
+  };
+  readonly id: string;
+  readonly title: string;
+  readonly comments?: (Comment | null)[] | null;
+  readonly content?: string | null;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+}
+
+type LazyTodo = {
+  readonly [__modelMeta__]: {
+    identifier: ManagedIdentifier<Todo, 'id'>;
+    readOnlyFields: 'createdAt' | 'updatedAt';
+  };
+  readonly id: string;
+  readonly title: string;
+  readonly comments: AsyncCollection<Comment>;
+  readonly content?: string | null;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+}
+
+export declare type Todo = LazyLoading extends LazyLoadingDisabled ? EagerTodo : LazyTodo
+
+export declare const Todo: (new (init: ModelInit<Todo>) => Todo) & {
+  copyOf(source: Todo, mutator: (draft: MutableModel<Todo>) => MutableModel<Todo> | void): Todo;
+}
+
+type EagerComment = {
+  readonly [__modelMeta__]: {
+    identifier: ManagedIdentifier<Comment, 'id'>;
+    readOnlyFields: 'createdAt' | 'updatedAt';
+  };
+  readonly id: string;
+  readonly postID: string;
+  readonly content: string;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+}
+
+type LazyComment = {
+  readonly [__modelMeta__]: {
+    identifier: ManagedIdentifier<Comment, 'id'>;
+    readOnlyFields: 'createdAt' | 'updatedAt';
+  };
+  readonly id: string;
+  readonly postID: string;
+  readonly content: string;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+}
+
+export declare type Comment = LazyLoading extends LazyLoadingDisabled ? EagerComment : LazyComment
+
+export declare const Comment: (new (init: ModelInit<Comment>) => Comment) & {
+  copyOf(source: Comment, mutator: (draft: MutableModel<Comment>) => MutableModel<Comment> | void): Comment;
+}
